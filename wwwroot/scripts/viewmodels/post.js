a827202a28e586b391d0131e823bc69e57cd95f8
PythEch <pythech.tr@gmail.com>
2017-12-15 21:06:31 +0100

    Merge post.js

--- wwwroot/scripts/viewmodels/post.js
+++ wwwroot/scripts/viewmodels/post.js
@@ -1,6 +1,23 @@
+const MAX_BODY_LENGTH = 500;
+
 define(['api', 'jquery', 'knockout'], function (api, $, ko) {
     function Post(post) {
-        console.log(post);
+
+        // Provide a lighter body in search view
+        if (post.lightView) {
+            // Clean out HTML code first
+            post.lightBody = post.body.replace(/<[^>]*>/g, '');
+            if (post.lightBody.length > MAX_BODY_LENGTH + 3)
+                post.lightBody = post.lightBody.slice(0, MAX_BODY_LENGTH) + '...';
+
+            // Highlight matches with surrounding characters
+            // Can lead to false-positives
+            let query = api.getSearchQuery();
+            for (let word of query.split(/\s/)) {
+                post.lightBody = post.lightBody.replace(new RegExp(`(\\w*${word}\\w*)`, 'ig'), '<b>$1</b>');
+            }
+        }
+
         this.post = ko.observable(post)
         this.tags = ko.observableArray([]);
         this.user = ko.observable({})
@@ -12,6 +29,7 @@ define(['api', 'jquery', 'knockout'], function (api, $, ko) {
             return false;
         }
         this.numberComments = ko.computed(() => this.comments().length)
+
         this.updatePost = () => {
             api.getTagforPost(this.post().id, (e) => {
                 this.tags(e)
@@ -19,26 +37,30 @@ define(['api', 'jquery', 'knockout'], function (api, $, ko) {
             api.getUserById(this.post().userId, e => {
                 this.user(e)
             })
-            api.getCommentsForPost(this.post().id, e => {
-                const userIdArray = e.map(e => e.userId);
-                const t = (c) => c.map(e => ({
-                    ...e,
-                    displayText: ko.computed(() => { 
-                        return e.text.length > 100 ? e.text.slice(0, 50) + ' ...' : e.text
-                    }),
-                }));
-                this.comments(t(e));
-                this.numberComments = ko.computed(() => this.comments.length)
-                this.commentsShowed(t(e.slice(0, 3)));                
-                api.getUsersForComments(userIdArray, u => {
-                    const userMap = e.map((e, i) => ({
+
+            // Don't load comments in search view
+            if (!this.lightView) {
+                api.getCommentsForPost(this.post().id, e => {
+                    const userIdArray = e.map(e => e.userId);
+                    const t = (c) => c.map(e => ({
                         ...e,
-                        user: u[i]
-                    }))
-                    this.comments(t(userMap));
-                    this.commentsShowed(t(userMap.slice(0, 3)));
+                        displayText: ko.computed(() => { 
+                            return e.text.length > 100 ? e.text.slice(0, 50) + ' ...' : e.text
+                        }),
+                    }));
+                    this.comments(t(e));
+                    this.numberComments = ko.computed(() => this.comments.length)
+                    this.commentsShowed(t(e.slice(0, 3)));                
+                    api.getUsersForComments(userIdArray, u => {
+                        const userMap = e.map((e, i) => ({
+                            ...e,
+                            user: u[i]
+                        }))
+                        this.comments(t(userMap));
+                        this.commentsShowed(t(userMap.slice(0, 3)));
+                    })
                 })
-            })
+            }
 
             const highlightCode = () => {
                 $('code').each(function (i, block) {
@@ -59,6 +81,12 @@ define(['api', 'jquery', 'knockout'], function (api, $, ko) {
         }
         if (post && post.hasOwnProperty('id'))
             this.updatePost()
+
+        // Gets Post object from post.js
+        this.goToQuestion = (post) => {
+            let postId = post.post().id; // this is the Post() object, it has a post property
+            document.location.assign(`#Question/postId`);
+        }
     }
 
     return Post;
